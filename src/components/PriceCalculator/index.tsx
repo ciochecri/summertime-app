2e89cb7fefb39d4c18af7078683496af37ca71c3
Newspada <leonardofacchini1997@gmail.com>
2025-04-29 21:21:15 +0200

    fix per sito

--- src/components/PriceCalculator/index.tsx
+++ src/components/PriceCalculator/index.tsx
@@ -14,13 +14,14 @@ const PriceCalculator = () => {
   };
 
   return (
-    <Col style={{alignItems: "center"}}>
+    <Col style={{alignItems: "center", marginTop: "2em"}}>
       <Slider
         min={1}
         max={7}
         onChange={onChange}
         value={typeof inputValue === 'number' ? inputValue : 0}
         tooltip={{ formatter }}
+        handleStyle={{width: "2.5em", height: "2.5em", marginTop: "-1em"}}
       />
       <PriceCalculatorPriceValue>
         <h6 style={{textDecoration : inputValue > 3 ? "line-through" : "none", textAlign: "center"}}>{inputValue * 130 + " €"}</h6>
